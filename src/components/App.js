39254dc60a2b6ead6bf8b0fb41427b8a3807fefb
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2020-02-24 11:52:43 +0545

    update order state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -25,11 +26,23 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Daily" />
+          <ul className="fishes">
+            {
+              Object.keys(this.state.fishes)
+                .map(key => <Fish key={key} index={key} addToOrder={this.addToOrder} details={this.state.fishes[key]} />)
+            }
+          </ul>
         </div>
         <Order />
         <Inventory addfish={this.addfish} loadSampleFishes={this.loadSampleFishes} />
